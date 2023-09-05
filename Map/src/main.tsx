d4ffd5f0013d0fc6cf8c5eeb721d4b71c75fc138
isaotoyama <86751458+isaotoyama@users.noreply.github.com>
2023-09-05 12:37:06 -0700

    init

---
+++ Map/src/main.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import "../style.css";
+import Experience from "../Experience/Experience";
+
+const experience = new Experience(document.querySelector(".experience-canvas"));
